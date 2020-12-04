d77a2054d41bee9c33d75be1c44303e585c9d851
Jess VanDerwalker <washu@sonic.net>
2020-12-04 15:06:56 -0800

    Refactor and type clean up to get request working

--- src/server.ts
+++ src/server.ts
@@ -3,17 +3,24 @@
  */
 
 import express from 'express';
+import { WeatherRequestHandler, WeatherResponse } from './weather_request_handler.js';
 
 const app = express();
 const port = 3030;
 
-app.get('/', (req, res) => {
-    res.send('This is the web server');
+app.get('/:stationId', (req, res) => {
+    // res.send('This is the web server');
+    const wrh: WeatherRequestHandler =
+        new WeatherRequestHandler(req.params.stationId, 0, 0, false);
+    wrh.getResponse()
+        .then((parsedRes) => {
+            res.send(parsedRes);
+        })
+        .catch((err) => {
+            res.send(err);
+        })
 });
 
-app.listen(port, (err) => {
-    if (err) {
-        console.error(err);
-    }
+app.listen(port, () => {
     console.log(`server is listening of ${port}`);
 });
--- src/weather_request_handler.ts
+++ src/weather_request_handler.ts
@@ -4,15 +4,7 @@
  * weather_request_handler.js
  */
 
-/**
- * Interface for a the weather request object type.
- */
-export interface WeatherRequest {
-    stationId: string;
-    x: number;
-    y: number;
-    siUnits: boolean;
-}
+import needle from 'needle';
 
 /**
  * Interface for the weather response object.
@@ -20,28 +12,70 @@ export interface WeatherRequest {
 export interface WeatherResponse {
     locationName: string;
     temperature: number;
-    humidity: number;
+    relativeHumidity: number;
 }
 
-const BASE_URL: string = 'https://api.weather.gov/stations/KPDX/observations/latest'
 /**
  * Class that handles the a request for weather data from NOAA JSON
  * API.
  */
 export class WeatherRequestHandler {
-    request: WeatherRequest;
+    stationId: string;
+    x: number;
+    y: number;
+    siUnits: boolean;
 
     /**
      * Create the request object.
-     * @param {WeatherRequest} request The request.
+     * @param stationId {String} The weather station id.
+     * @param x {Number} The grid x of the station.
+     * @param y {Number} The grid y of the station.
+     * @param siUnits {Boolean} Return values in SI units if true.
      */
-    constructor(request: WeatherRequest) {
-        this.request = request;
+    constructor(stationId: string, x: number, y: number, siUnits: boolean) {
+        this.stationId = stationId;
+        this.x = x;
+        this.y = y;
+        this.siUnits = siUnits;
     }
 
-    getWeather(): WeatherResponse {
-        let response: WeatherResponse;
+    /**
+     * Get the response from with the weather.
+     * @return {Promise} Promise that resolves when data is retrieved
+     * and parsed.
+     */
+    getResponse(): Promise<WeatherResponse> {
+        if (!this.stationId) {
+            return null;
+        }
+        let url = 'https://api.weather.gov/stations/' +
+            this.stationId + '/observations/latest';
+        const respPromise: Promise<WeatherResponse> =
+            new Promise((resolve, reject) => {
+                needle('get', url)
+                    .then((resp) => {
+                        resolve(this._parseResponse(resp.body));
+                    })
+                    .catch((err) => {
+                        reject(err);
+                    });
+            });
+        return  respPromise;
+    }
 
-        return response;
+    /**
+     * Parse the response and just get out the data that we need.
+     * @param body The body of the response from NOAA
+     * @return {WeatherResponse} Response with parsed data.
+     */
+    private _parseResponse(body: string): WeatherResponse {
+        let stationRes = JSON.parse(body);
+        let parsedRes: WeatherResponse = {
+            temperature: stationRes.properties.temperature.value,
+            relativeHumidity: stationRes.properties.relativeHumidity.value,
+            locationName: stationRes.properties.textDescription
+        };
+
+        return parsedRes;
     }
 }
